f6cbbb3303fba5c2768afbeb1b2f19148ae49e5f
Fred Bergman <fred@fbergman.se>
2014-02-27 14:33:04 +0100

    Cleanup

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -2,18 +2,18 @@ ActiveAdmin.register Project do
   actions :index, :update, :edit, :new, :create, :destroy
   menu priority: 1
 
-  form html: { enctype: "multipart/form-data" } do |f|
+  form html: { enctype: 'multipart/form-data' } do |f|
     f.semantic_errors *f.object.errors.keys
-    f.inputs "Film Details" do
+    f.inputs 'Project details' do
       f.input :title
       f.input :body
       f.has_many :images do |image|
-        image.inputs "Image" do
+        image.inputs 'Image' do
           image.input :description
           image.input :credit
           image.input :position
           image.input :attachment, as: :file, hint: image.template.image_tag(image.object.attachment.url(:small)), required: true
-          image.input :_destroy, as: :boolean, required: false, label: "Remove"
+          image.input :_destroy, as: :boolean, required: false, label: 'Remove'
         end
       end
     end
