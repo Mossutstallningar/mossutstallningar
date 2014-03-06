b543af1f6cbeafb5f7bca05113f0626bc7f5ab9f
Fred Bergman <fred@fbergman.se>
2014-03-06 17:10:06 +0100

    Add basic page and dashboard layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :load_global_resources
+
+  def load_global_resources
+    @projects = Project.all
+  end
 end
