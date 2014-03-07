2c06ae43de3cb16c47aa1c30afa760f3d9aa1798
Fred Bergman <fred@fbergman.se>
2014-03-07 15:41:38 +0100

    Add basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate if ENV["AUTHENTICATE"] == "true"
   before_action :load_global_resources
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["HTTP_AUTH_USER"] && password == ENV["HTTP_AUTH_PASS"]
+    end
+  end
 
   def load_global_resources
     @projects = Project.all
