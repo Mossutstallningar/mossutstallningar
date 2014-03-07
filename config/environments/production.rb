9951a6461b45da9d166cd9df353853ba7c2b48cd
Fred Bergman <fred@fbergman.se>
2014-03-07 16:34:51 +0100

    Fix Modernizr path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Mossutstallningar::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( modernizr.js )
+  config.assets.precompile += %w( libs/modernizr.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
