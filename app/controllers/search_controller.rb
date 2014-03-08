6869d5ed963958a652b55e976c6675e55dc22fb7
Fred Bergman <fred@fbergman.se>
2014-03-08 23:30:39 +0100

    Render search result on a page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @projects = Project.search(params[:q])
+    @result = Project.search(params[:q])
 
     respond_to do |format|
       format.html
