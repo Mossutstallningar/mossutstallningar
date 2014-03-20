337ea0fddbb2f8489595005bfe593bdf455fac7f
Fred Bergman <fred@fbergman.se>
2014-03-20 09:26:35 +0100

    Set default locale after skiping validation

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,11 @@ module Mossutstallningar
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Stockholm'
 
+    # Skip validation of locale
+    config.i18n.enforce_available_locales = false
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :sv
-
-    # Skip validation of locale
-    config.i18n.enforce_available_locales = false
   end
 end
