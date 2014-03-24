bf85c747c053bfc752507123552cb6ffd4b1cae8
Fred Bergman <fred@fbergman.se>
2014-03-24 10:40:13 +0100

    Don't let Slim minify html in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Mossutstallningar::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Don't let Slim minify html
+  Slim::Engine.set_default_options pretty: true
 end
