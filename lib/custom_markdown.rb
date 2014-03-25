56c6c4365a13a0ff2db269959a911fb09b1b166a
Fred Bergman <fred@fbergman.se>
2014-03-25 08:25:33 +0100

    Add comment on match pattern

--- lib/custom_markdown.rb
+++ lib/custom_markdown.rb
@@ -1,5 +1,6 @@
 class CustomMarkdown < Redcarpet::Render::HTML
   def preprocess(full_document)
+    # match [image:src|alt|caption]
     full_document.gsub(/\[image:(.*)\|(.*)\|(.*)\]/) do
       html = ''
       html << %-<img src="#{$1}" alt="#{$2}">-
