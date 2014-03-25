4f43b65844bb32aaaf6edc038fa265a17a3e9263
Fred Bergman <fred@fbergman.se>
2014-03-25 09:01:44 +0100

    Add image with caption styles

--- lib/custom_markdown.rb
+++ lib/custom_markdown.rb
@@ -3,8 +3,10 @@ class CustomMarkdown < Redcarpet::Render::HTML
     # match [image:src|alt|caption]
     full_document.gsub(/\[image:(.*)\|(.*)\|(.*)\]/) do
       html = ''
-      html << %-<img src="#{$1}" alt="#{$2}">-
-      html << "<span>#{$3}</span>"
+      html << '<span class="image-with-caption">'
+      html << %Q|<img src="#{$1}" alt="#{$2}" class="image-with-caption-image">|
+      html << %Q|<span class="image-with-caption-caption">#{$3}</span>|
+      html << '</span>'
 
       html
     end
