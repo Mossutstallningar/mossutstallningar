02958044fe565891f2e0926c33fb74ffaa7775e0
Fred Bergman <fred@fbergman.se>
2014-03-26 09:42:37 +0100

    Refactoring

--- lib/custom_markdown.rb
+++ lib/custom_markdown.rb
@@ -1,29 +1,41 @@
 class CustomMarkdown < Redcarpet::Render::HTML
   def initialize(*args)
     @resource = args[0][:resource]
-
     super
   end
 
-  def preprocess(full_document)
-    if @resource.present? && @resource.images.present?
-      # match [images]
-      full_document.gsub!(/(\[images\])/) do
-        html = ''
-        html << '<ul class="image-chaos">'
+  def preprocess(content)
+    content = content_with_image_chaos(content) if has_resource?
+    content = content_with_caption_images(content)
 
-        @resource.images.each do |image|
-          html << '<li class="image-chaos-item">'
-          html << %Q|<img src="#{image.small}" alt="#{image.description}" class="image-chaos-image">|
-          html << '</li>'
-        end
+    content
+  end
 
-        html << '</ul>'
+  private
+
+  def has_resource?
+    @resource.present? && @resource.images.present?
+  end
+
+  def content_with_image_chaos(content)
+    # match [images]
+    content.gsub(/(\[images\])/) do
+      html = ''
+      html << '<ul class="image-chaos">'
+
+      @resource.images.each do |image|
+        html << '<li class="image-chaos-item">'
+        html << %Q|<img src="#{image.small}" alt="#{image.description}" class="image-chaos-image">|
+        html << '</li>'
       end
-    end
 
+      html << '</ul>'
+    end
+  end
+
+  def content_with_caption_images(content)
     # match [image:src|alt|caption]
-    full_document.gsub(/\[image:(.*)\|(.*)\|(.*)\]/) do
+    content.gsub(/\[image:(.*)\|(.*)\|(.*)\]/) do
       html = ''
       html << '<span class="image-with-caption">'
       html << %Q|<img src="#{$1}" alt="#{$2}" class="image-with-caption-image">|
